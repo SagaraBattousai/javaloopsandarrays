c580bff3d00840711dfed246b8505a86a6edb312
jam414 <james.manzoor14@imperial.ac.uk>
2014-11-28 00:35:18 +0000

    Can't get horrisontal lines to line up so I'll reduce the code and leave it unaligned

--- LoopArraysProgram.java
+++ LoopArraysProgram.java
@@ -11,13 +11,25 @@ public class LoopArraysProgram {
       values[i] = IOUtil.readDouble(); 
     }
     System.out.println("Please enter the minimum and maximum values" +
-       " respectivly followed by the number of ranges (Buckets) :")
+       " respectivly followed by the number of ranges (Buckets) :");
 
     double min = IOUtil.readDouble();
     double max = IOUtil.readDouble();
     int numBuckets = IOUtil.readInt();
-
-  
+    double inc = (Math.abs(max - min)) / numBuckets;
+    int f = 0;
+    int[] freq = LoopArraysLibrary.frequencyTable(min, max, numBuckets,
+        values);
+//    double [][] buckets = new double [numBuckets][2]; //2 is for the two limits
+//  public static void       
+    System.out.println("Bucket|Frequency (i)");
+    System.out.println("============================");
+   //for(int i = 0; i < numBuckets; i++){
+      for(double j = min; j < max; j += inc){
+        System.out.print(j + " <= i < " + (j + inc) + " ");
+        System.out.println( " | " + freq [f]);
+        f++;
+      }
   
   
   
@@ -28,37 +40,6 @@ public class LoopArraysProgram {
   
   
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   
   
