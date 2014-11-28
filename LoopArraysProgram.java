362c8176ff9416e67b41e85d9bf1c2e3bdcf7b39
jam414 <james.manzoor14@imperial.ac.uk>
2014-11-28 12:32:25 +0000

    Do some test cases.

--- LoopArraysProgram.java
+++ LoopArraysProgram.java
@@ -7,9 +7,11 @@ public class LoopArraysProgram {
 
     int n = IOUtil.readInt();
     double[] values = new double[n];
-    for(int i = 0; i < n; i++){
+    f
+      or(int i = 0; i < n; i++){
       values[i] = IOUtil.readDouble(); 
     }
+
     System.out.println("Please enter the minimum and maximum values" +
        " respectivly followed by the number of ranges (Buckets) :");
 
@@ -20,45 +22,37 @@ public class LoopArraysProgram {
     int f = 0;
     int[] freq = LoopArraysLibrary.frequencyTable(min, max, numBuckets,
         values);
-
-   // printTable(numBuckets);
-   // printHist(
- // }    
- // private static void printTable( {
+// Table Generation, pointless todo in a method as its arbitary what 
+// arguments is send.
     System.out.println("Bucket|Frequency (i)");
     System.out.println("============================");
-   //for(int i = 0; i < numBuckets; i++){
       for(double j = min; j < max; j += inc){
         System.out.println(j + " <= i < " + (j + inc) + " | " + 
             freq [f]);
         f++;
       }
     
-  
-/*private static void printHist(   */ //{
+// Histogram Generation No method as per previous explanation.
       System.out.println("");
       for(int i = LoopArraysLibrary.maximum(freq); i > 0; i--){
-      String gaps = "     ";
-      System.out.print(i + " |");
+        String gaps = "     ";
+        System.out.print(i + " |");
         for(int j = 0; j < numBuckets; j++){
           if (freq[j] >= i){
             System.out.print("    ##    ");
-   //         gaps += "    ";
           } else {
             System.out.print("          ");
-
-       //   gaps += "      ";
           }
+        }
+        System.out.println("");
       }
-      System.out.println("");
-          }
       System.out.println("  +---------------------------------------------" +
          "----------------------->");
       System.out.print("   ");
       for(double j = min; j < max; j += inc){
         System.out.print(j + "_" + (j + inc) + " | ");
       } 
-     System.out.println(""); 
+      System.out.println(""); 
 
 
 
